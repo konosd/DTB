a1f456d53d766920967ad4ce9e40bb246f4b1ff5
konosd <33687738+konosd@users.noreply.github.com>
2018-12-10 16:48:50 +0200

    Add files via upload

---
+++ initializer.py
@@ -0,0 +1,12 @@
+import os
+
+# #########
+# Initializer
+# #########
+
+data_dir = os.path.join(os.getcwd(),'data')
+source_dir = os.path.join(os.getcwd(),'code')
+
+pretty_data = os.path.join(source_dir,'pretty_data.py')
+print('Parsing and tidying the DTC database, sir.')
+exec(open(pretty_data).read())
